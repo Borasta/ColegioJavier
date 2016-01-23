1b0ed185e1bf645a780da6c9862de6d5bbc9bd26
Borasta <orlandodavid.01@gmail.com>
2016-01-23 00:10:31 -0430

    Se añadio una imagen parallax y se mejoro un poco el diseño de la pagina

--- render.js
+++ render.js
@@ -1,35 +1,36 @@
-"use strict";
-
-module.exports = function (json) {
+module.exports = function( json ) {
 	/* 
- 	{
- 		res: res,	// Object response
- 		tmp: path,	// Path of template
- 		ctn: path,	// Path of content
- 		values: {	//` Values to bind
- 			// values
- 		}
- 	}
- */
+		{
+			res: res,	// Object response
+			tmp: path,	// Path of template
+			ctn: path,	// Path of content
+			values: {	//` Values to bind
+				// values
+			}
+		}
+	*/
 
 	var fs = require("fs");
 	var bind = new RegExp(/[^\{{2}]+(?=\}{2})/g);
 
 	var htmlString = fs.readFileSync(json.tmp).toString();
 	var cont = fs.readFileSync(json.ctn).toString();
-
+	
 	var bindings = htmlString.match(bind);
 
 	// Add content to htmlString
-	for (var i = 0; i < bindings.length; i++) {
-		if (bindings[i].indexOf("content") >= 0) htmlString = htmlString.replace("{{" + bindings[i] + "}}", cont);
-	}bindings = htmlString.match(bind);
+	for( var i = 0; i < bindings.length; i++ )
+		if( bindings[i].indexOf("content") >= 0 )
+			htmlString = htmlString.replace("{{" + bindings[i] + "}}", cont );
+
+	bindings = htmlString.match(bind);
 
 	// Add values to htmlString
-	for (var i = 0; bindings && i < bindings.length; i++) {
-		var valor = json.values[bindings[i].trim()];
-		if (valor) htmlString = htmlString.replace("{{" + bindings[i] + "}}", valor);
+	for( var i = 0; (bindings) && (i < bindings.length); i++ ) {
+		var valor = json.values[ bindings[i].trim() ];
+		if( valor )
+			htmlString = htmlString.replace("{{" + bindings[i] + "}}", valor );
 	}
 
 	json.res.write(htmlString);
-};
+}
